b37c21fc46053ae619ef0d68fa417854dd2bb95e
pnxguide <yuttapichai.ke@gmail.com>
2024-07-13 19:49:53 +0000

    updated scan

--- src/operator/op_scan.cpp
+++ src/operator/op_scan.cpp
@@ -4,29 +4,6 @@
 #include "../catalog/catalog.hpp"
 #include "operator.hpp"
 
-std::vector<std::string> get_column_names(std::string table_path) {
-    std::vector<std::string> column_names;
-
-    // TODO: Get the first row and split into strings delimited by ,
-
-    return column_names;
-}
-
-uint64_t get_table_size(std::string table_path) {
-    uint64_t row_count = 0;
-
-    std::string line;
-    std::ifstream table_file(table_path);
-    if (table_file.is_open()) {
-        while (std::getline(table_file, line)) {
-            row_count++;
-        }
-        table_file.close();
-    }
-
-    return row_count;
-}
-
 OpScan::OpScan(std::string table_name) {
     Catalog &catalog = Catalog::get_instance();
 
